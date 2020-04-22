dc1ef6e9b24e0efb6b516f6c18a95d0728833f7c
anten-li <anten.83@mail.ru>
2020-04-22 22:21:24 +0300

    ver 1

---
+++ CMake_test/CMake_test.cpp
@@ -0,0 +1,27 @@
+﻿// CMake_test.cpp : Defines the entry point for the application.
+//
+
+#define UNICODE
+
+#include "CMake_test.h"
+
+HANDLE hstdOUT;
+//HANDLE hstdIN;
+//TCHAR  buff[256];
+
+int main()
+{
+    hstdOUT = GetStdHandle(STD_OUTPUT_HANDLE);
+    //	hstdIN  = GetStdHandle(STD_INPUT_HANDLE);
+
+    WriteLn(toTCHAR("тест c++"));
+ 
+    return 0;
+}
+
+void WriteLn(LPTCH str)
+{
+    DWORD Writen, endStr = CRLF;
+    WriteConsole(hstdOUT, str, lstrlen(str), &Writen, NULL);
+    WriteConsole(hstdOUT, &endStr, 2, &Writen, NULL);
+}
---
+++ CMake_test/CMake_test.h
@@ -0,0 +1,16 @@
+﻿// CMake_test.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <Windows.h>
+
+#ifdef UNICODE
+#define CRLF 0x000a000d
+#define toTCHAR(x) L##x 
+#else
+#define CRLF 0x00000a0d
+#define toTCHAR(x) x
+#endif // UNICODE
+
+void WriteLn(LPTCH str);
